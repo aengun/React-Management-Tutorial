f4c96ab6a69282d2353eafb870482f14ae0518f8
aengun <aengun@icloud.com>
2019-11-05 20:21:10 +0900

    Creat React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+     <img src={logo} lat = "logo"/>
+     <h2> 김학준 병신! 쮜지리~ </h2>
+    </div>
+  );
+}
+
+export default App;
